d2c75634ce6ae2d9b8e3d9affb8164bf281b9a17
alinebarreto <aline.barreto98@hotmail.com>
2026-03-31 10:08:29 -0400

    Segundo commit: inserção de imagem na tela

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 //material: estrutura padronizada - importante lembrar que é uma estrutura por padrão do android
 
 void main() {
-  runApp(
-    EcoradarApp(),
-  ); 
+  runApp(EcoradarApp());
   // runApp sempre precisa ter, pois é a que chama a aplicação para funcionar
   //seguido do nome da aplicação.
 }
@@ -30,6 +28,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Ecoradar"), centerTitle: true));
+    return Scaffold(
+      appBar: AppBar(title: Text("Ecoradar"), centerTitle: true),
+      body: Column(
+        children: [
+          SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset(
+              'assets/images/ecoradar.png',
+              height: 250,
+              width: 250,
+            ),
+          ),
+          Text(
+            "Conheça, Inspire, Preserve",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
   }
 }
